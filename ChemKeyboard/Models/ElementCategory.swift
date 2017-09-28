538db327ccf53e19f963cebb1673b82e484f8ac6
Andrey Ogrenich <andrey.ogrenich@chemopin.tech>
2017-09-28 14:16:50 +0300

    Add `Element` and `ElementCategory` models + `Symbol` and `SymbolGroup` typealiases

---
+++ ChemKeyboard/Models/ElementCategory.swift
@@ -0,0 +1,32 @@
+//
+//  ElementCategory.swift
+//  ChemKeyboard
+//
+//  Created by Andrey Ogrenich on 28/09/2017.
+//  Copyright © 2017 Chemistry Open Intelligence Pte. Ltd. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+import ObjectMapper
+import ObjectMapper_Realm
+
+class ElementCategory: Object, Mappable {
+    
+    @objc dynamic var name: String? = nil
+    @objc dynamic var color: String? = nil
+    
+    var elements = List<Element>()
+    
+    
+    required convenience init?(map: Map) {
+        self.init()
+    }
+    
+    func mapping(map: Map) {
+        name        <- map["name"]
+        color       <- map["color"]
+        elements    <- (map["elements"], ListTransform<Element>())
+    }
+    
+}
---
+++ ChemKeyboard/Models/SymbolGroup.swift
@@ -0,0 +1,11 @@
+//
+//  SymbolGroup.swift
+//  ChemKeyboard
+//
+//  Created by Andrey Ogrenich on 28/09/2017.
+//  Copyright © 2017 Chemistry Open Intelligence Pte. Ltd. All rights reserved.
+//
+
+import Foundation
+
+typealias SymbolGroup = [Symbol]
