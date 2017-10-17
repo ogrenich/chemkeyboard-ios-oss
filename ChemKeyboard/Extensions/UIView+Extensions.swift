a14b47e60d2da6b4ee4170e0f2384bf846dc9b7d
Maria Dagaeva <maria.dagaeva@chemopin.tech>
2017-10-17 12:45:29 +0300

    Adds IBInspectable UIView extensions `shadowRadius`, `shadowOpacity`, `shadowOffset` and `shadowColor`

--- ChemKeyboard/Extensions/UIView+Extensions.swift
+++ ChemKeyboard/Extensions/UIView+Extensions.swift
@@ -37,3 +37,60 @@ public extension UIView {
     }
     
 }
+
+public extension UIView {
+    
+    @IBInspectable
+    public var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    public var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+            if newValue > 0 {
+                layer.masksToBounds = false
+                
+                /// Defining shadow's path for performance improvement
+                layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius).cgPath
+            }
+        }
+    }
+    
+    @IBInspectable
+    public var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable
+    public var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.shadowColor = color.cgColor
+            } else {
+                layer.shadowColor = nil
+            }
+        }
+    }
+    
+}
