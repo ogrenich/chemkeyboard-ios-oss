274ece37c7be3a20ef200e6d87361c04783cbcbc
Maria Dagaeva <maria.dagaeva@chemopin.tech>
2017-10-19 15:29:56 +0300

    Adds layout of `SimpleElementPopUp` and `ExtendedElementPopUp`

--- Keyboard/PopUp/PopUp.swift
+++ Keyboard/PopUp/PopUp.swift
@@ -29,11 +29,72 @@ class PopUp {
 extension PopUp {
     
     func show(element: Element, at frame: CGRect, style: PopUpStyle) {
+        hide()
+        
+        switch style {
+        case .extended:
+            if frame.origin.y - 70 < 0 {
+                fallthrough
+            }
+            
+            let frameForPopUp = CGRect(x: frame.origin.x - 15,
+                                       y: frame.origin.y - 70,
+                                       width: 88, height: 114)
+            popUpView = UIView(frame: frameForPopUp)
+            addContentView(with: element, style: .extended)
+        case .simple:
+            let frameForPopUp = CGRect(x: frame.origin.x - 8,
+                                       y: frame.origin.y - 40,
+                                       width: 74, height: 84)
+            popUpView = UIView(frame: frameForPopUp)
+            addContentView(with: element, style: .simple)
+        }
+        
+        setUpShadow(with: element.category?.shadowColor?.hexColor ?? .black,
+                    radius: 14, offset: CGSize(width: 0, height: 1))
+        keyboardView.addSubview(popUpView)
     }
     
     func show(symbol: Symbol, at frame: CGRect, style: PopUpStyle) {
     
     func hide() {
+        contentView?.removeFromSuperview()
+        popUpView?.removeFromSuperview()
+    }
+    
+}
+
+private extension PopUp {
+    
+    func addConstraints() {
+        popUpView.addSubview(contentView)
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.topAnchor.constraint(equalTo: popUpView.topAnchor).isActive = true
+        contentView.leadingAnchor.constraint(equalTo: popUpView.leadingAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: popUpView.bottomAnchor).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: popUpView.trailingAnchor).isActive = true
+    }
+    
+    func addContentView(with element: Element, style: PopUpStyle) {
+        switch style {
+        case .simple:
+            contentView = SimpleElementPopUp(frame: popUpView.bounds).configure(with: element)
+        case .extended:
+            contentView = ExtendedElementPopUp(frame: popUpView.bounds).configure(with: element)
+        }
+        
+        addConstraints()
+    }
+    
+    func setUpShadow(with color: UIColor, radius: CGFloat, offset: CGSize) {
+        popUpView.shadowRadius = radius
+        popUpView.shadowColor = color
+        popUpView.shadowOffset = offset
+        popUpView.shadowOpacity = 1
+        
+        if let path = (contentView.layer.mask as? CAShapeLayer)?.path {
+            popUpView.layer.shadowPath = path
+        }
     }
     
 }
--- Keyboard/PopUp/SimpleElementPopUp/SimpleElementPopUp.swift
+++ Keyboard/PopUp/SimpleElementPopUp/SimpleElementPopUp.swift
@@ -10,5 +10,53 @@ import UIKit
 
 class SimpleElementPopUp: UIView {
 
+    fileprivate let symbolLabel: UILabel = UILabel()
+    fileprivate let numberLabel: UILabel = UILabel()
+
 }
 
+extension SimpleElementPopUp {
+    
+    func configure(with element: Element) -> SimpleElementPopUp {
+        backgroundColor = element.category?.color?.hexColor
+        
+        accessoryShape(radius: 4, heightOfCell: 44, widthOfCell: 58)
+        
+        setUpLabels(with: element)
+        
+        return self
+    }
+    
+}
+
+private extension SimpleElementPopUp {
+    
+    func setUpLabels(with element: Element) {
+        
+        symbolLabel.font = UIFont(name: "SFUIDisplay-Medium", size: 21)
+        numberLabel.font = UIFont(name: "SFUIDisplay-Bold", size: 10)
+        
+        symbolLabel.text = element.symbol?.value ?? ""
+        numberLabel.text = element.number.value != nil ? "\(element.number.value!)" : ""
+        symbolLabel.textColor = element.category?.textColor?.hexColor ?? .black
+        numberLabel.textColor = (element.category?.textColor?.hexColor ?? .black).withAlphaComponent(0.5)
+        
+        [symbolLabel, numberLabel].forEach { label in
+            label.adjustsFontSizeToFitWidth = true
+            
+            addSubview(label)
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.topAnchor.constraint(equalTo: topAnchor, constant: 3).isActive = true
+        }
+        
+        symbolLabel.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        numberLabel.heightAnchor.constraint(equalToConstant: 12).isActive = true
+        
+        symbolLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 6).isActive = true
+        numberLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -6).isActive = true
+        
+        numberLabel.leadingAnchor.constraint(greaterThanOrEqualTo: symbolLabel.trailingAnchor,
+                                             constant: 2).isActive = true
+    }
+    
+}
