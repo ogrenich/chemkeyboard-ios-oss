19a8ba7ee69c792c4289815bae2e68ee28a4615f
Andrey Ogrenich <andrey.ogrenich@chemopin.tech>
2017-10-23 15:37:44 +0300

    Adds static `instance` for `AppDelegate`

--- ChemKeyboard/AppDelegate.swift
+++ ChemKeyboard/AppDelegate.swift
@@ -11,6 +11,10 @@ import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    static let instance: AppDelegate = {
+        return UIApplication.shared.delegate as! AppDelegate
+    }()
 
     var window: UIWindow?
 
