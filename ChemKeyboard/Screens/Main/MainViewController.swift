32a1a09b03cecde2eb86aa0f77700b9b7b640fa0
Andrey Ogrenich <andrey.ogrenich@chemopin.tech>
2017-10-23 15:37:08 +0300

    Adds layout of `Main` Screen

--- ChemKeyboard/Screens/Main/MainViewController.swift
+++ ChemKeyboard/Screens/Main/MainViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, Storyboardable {
+    
+    @IBOutlet weak var infoButton: Button!
+    @IBOutlet weak var formulaTextField: TextField!
+    @IBOutlet weak var clearTextButton: Button!
+    @IBOutlet weak var copyToClipboardButton: Button!
+    @IBOutlet weak var versionLabel: UILabel!
+    @IBOutlet weak var copyrightLabel: UILabel!
+    
+    
+    fileprivate let bag = DisposeBag()
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        bindSelf()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        formulaTextField.becomeFirstResponder()
     }
-
 
 }
 
+private extension MainViewController {
+    
+    func bindSelf() {
+        formulaTextField.rx.text
+            .map { $0 == nil || $0?.count == 0 }
+            .bind(to: clearTextButton.rx.isHidden)
+            .disposed(by: bag)
+        
+        clearTextButton.rx.tap
+            .map { _ in nil }
+            .bind(to: formulaTextField.rx.text)
+            .disposed(by: bag)
+        
+        clearTextButton.rx.tap
+            .map { _ in true }
+            .bind(to: clearTextButton.rx.isHidden)
+            .disposed(by: bag)
+        
+        copyToClipboardButton.rx.tap
+            .withLatestFrom(formulaTextField.rx.text)
+            .bind { UIPasteboard.general.string = $0 }
+            .disposed(by: bag)
+    }
+    
+}
