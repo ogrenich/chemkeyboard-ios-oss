d4fbccfd494d738a3a83858c89f0a94c22c3c618
Andrey Ogrenich <andrey.ogrenich@chemopin.tech>
2017-10-23 12:33:59 +0300

    Renames `ViewController` to `MainViewController`

--- ChemKeyboard/Screens/Main/MainViewController.swift
+++ ChemKeyboard/Screens/Main/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  ChemKeyboard
 //
 //  Created by Andrey Ogrenich on 25/09/2017.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
