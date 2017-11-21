7ada5207e6842a0fa5367fba0b93587a3a5bb22b
Maria Dagaeva <maria.dagaeva@chemopin.tech>
2017-11-21 19:23:18 +0300

    Adds `switchButton`, `spaceButton`, `returnButton` & `deleteButton` at `KeyboardActionsTableViewCell`

--- Keyboard/Screens/Keyboard/Views/KeyboardActionsTableViewCell/KeyboardActionsTableViewCell.swift
+++ Keyboard/Screens/Keyboard/Views/KeyboardActionsTableViewCell/KeyboardActionsTableViewCell.swift
@@ -15,15 +15,30 @@ import Neon
 @IBDesignable
 class KeyboardActionsTableViewCell: UITableViewCell {
     
+    lazy var switchButton: UIButton = UIButton()
+    fileprivate lazy var spaceButton: UIButton = UIButton()
+    lazy var returnButton: UIButton = UIButton()
+    fileprivate lazy var deleteButton: UIButton = UIButton()
     fileprivate lazy var collectionView: UICollectionView = UICollectionView(frame: .zero,
                                                                              collectionViewLayout:
-                                                                             UICollectionViewFlowLayout())    
+                                                                             UICollectionViewFlowLayout())
     
+    fileprivate lazy var spaceSymbol: Symbol = Symbol()
+    fileprivate lazy var returnSymbol: Symbol = Symbol()
+
+    
+    fileprivate weak var needsReactToSimpleButtonTouchEvent: PublishSubject<Symbol?>!
+    fileprivate weak var needsReactToDeleteButtonTouchEvent: PublishSubject<Void>!
     fileprivate weak var needsPlayInputClick: PublishSubject<Void>!
     
     
     fileprivate var bag = DisposeBag()
     fileprivate var viewModel: KeyboardActionsTableViewCellModel!
+    
+    
+    fileprivate var subviewsHierarchyHasBeenConfigured: Bool = false
+    
+    fileprivate weak var timer: Timer?
     
     
     override func prepareForReuse() {
@@ -35,8 +50,13 @@ class KeyboardActionsTableViewCell: UITableViewCell {
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        collectionView.fillSuperview()
+        layoutFrames()
     }
+    
+    deinit {
+        invalidateTimer()
+    }
+
         
 }
 
@@ -44,13 +64,23 @@ extension KeyboardActionsTableViewCell {
     
     @discardableResult
     func configure(with viewModel: KeyboardActionsTableViewCellModel,
-                   _ needsPlayInputClick: PublishSubject<Void>) -> KeyboardActionsTableViewCell {
+                   _ needsPlayInputClick: PublishSubject<Void>,
+                   needsReactToDeleteButtonTouchEvent: PublishSubject<Void>,
+                   needsReactToSimpleButtonTouchEvent: PublishSubject<Symbol?>) -> KeyboardActionsTableViewCell {
         self.viewModel = viewModel
         self.needsPlayInputClick = needsPlayInputClick
+        self.needsReactToDeleteButtonTouchEvent = needsReactToDeleteButtonTouchEvent
+        self.needsReactToSimpleButtonTouchEvent = needsReactToSimpleButtonTouchEvent
         
         setupUI()
         
+        configureButtons()
+        
         configureCollectionView()
+        
+        if !subviewsHierarchyHasBeenConfigured {
+            configureSubviewsHierarchy()
+        }
         
         bindSelf()
         bindToViewModel()
@@ -72,10 +102,6 @@ private extension KeyboardActionsTableViewCell {
 private extension KeyboardActionsTableViewCell {
     
     func configureCollectionView() {
-        if collectionView.superview == nil {
-            addSubview(collectionView)
-        }
-        
         collectionView.register(KeyboardActionsCollectionViewCell.self)
         
         guard let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else {
@@ -97,6 +123,38 @@ private extension KeyboardActionsTableViewCell {
         layout.minimumInteritemSpacing = 0
         layout.minimumLineSpacing = 6
     }
+    
+    func configureButtons() {
+        switchButton.setImage(UIImage(named: "Switch"), for: .normal)
+        switchButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: 6, bottom: 4, right: 0)
+        
+        spaceButton.setTitle("SPACE", letterSpacing: 1.1)
+        spaceSymbol.value = " "
+        
+        returnSymbol.value = "\n"
+        
+        [spaceButton, returnButton].forEach { button in
+            button.backgroundColor = #colorLiteral(red: 0.9568627451, green: 0.9568627451, blue: 0.9568627451, alpha: 1)
+            button.cornerRadius = 4
+            button.titleLabel?.font = UIFont(name: "SFUIDisplay-Bold", size: 12)
+            button.setTitleColor(.black, for: .normal)
+        }
+        
+        deleteButton.setImage(UIImage(named: "Delete"), for: .normal)
+        deleteButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 4, right: 4)
+        
+        addGestureRecognizersOnDeleteButton()
+    }
+    
+    func configureSubviewsHierarchy() {
+        addSubview(switchButton)
+        addSubview(spaceButton)
+        addSubview(returnButton)
+        addSubview(deleteButton)
+        addSubview(collectionView)
+        
+        subviewsHierarchyHasBeenConfigured = true
+    }
         
 }
 
@@ -106,6 +164,28 @@ private extension KeyboardActionsTableViewCell {
         collectionView.rx.itemHighlighted
             .map { _ in }
             .bind(to: needsPlayInputClick)
+            .disposed(by: bag)
+        
+        switchButton.rx.tap
+            .bind(to: needsPlayInputClick)
+            .disposed(by: bag)
+        
+        spaceButton.rx.tap
+            .bind(to: needsPlayInputClick)
+            .disposed(by: bag)
+        
+        returnButton.rx.tap
+            .bind(to: needsPlayInputClick)
+            .disposed(by: bag)
+        
+        spaceButton.rx.tap
+            .map { [weak self] in self?.spaceSymbol }
+            .bind(to: needsReactToSimpleButtonTouchEvent)
+            .disposed(by: bag)
+        
+        returnButton.rx.tap
+            .map { [weak self] in self?.returnSymbol }
+            .bind(to: needsReactToSimpleButtonTouchEvent)
             .disposed(by: bag)
     }
 
@@ -141,6 +221,64 @@ private extension KeyboardActionsTableViewCell {
                 self?.collectionView.reloadData()
             })
             .disposed(by: bag)
+    }
+    
+}
+
+private extension KeyboardActionsTableViewCell {
+    
+    func layoutFrames() {
+            collectionView.fillSuperview(left: 0, right: 0, top: 0, bottom: 50)
+            switchButton.fillSuperview(left: 0, right: width - 44, top: 40, bottom: 0)
+            spaceButton.fillSuperview(left: 50, right: 47 + (width - 88) * 0.36, top: 40, bottom: 6)
+            returnButton.fillSuperview(left: 47 + (width - 88) * 0.64, right: 50, top: 40, bottom: 6)
+            deleteButton.fillSuperview(left: width - 44, right: 0, top: 40, bottom: 0)
+    }
+    
+}
+
+private extension KeyboardActionsTableViewCell {
+    
+    func setUpTimer(with selector: Selector) {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self,
+                                     selector: selector,
+                                     userInfo: nil, repeats: true)
+    }
+    
+    func invalidateTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+}
+
+extension KeyboardActionsTableViewCell {
+    
+    func addGestureRecognizersOnDeleteButton() {
+        let tap = UITapGestureRecognizer(target: self,
+                                         action: #selector(handleSimpleDeleteEvent))
+        
+        let longPress = UILongPressGestureRecognizer(target: self,
+                                                     action: #selector(handleLongPressOnDeleteButton(_:)))
+        
+        deleteButton.addGestureRecognizer(tap)
+        deleteButton.addGestureRecognizer(longPress)
+    }
+    
+    @objc func handleSimpleDeleteEvent() {
+        needsReactToDeleteButtonTouchEvent.onNext(())
+        needsPlayInputClick.onNext(())
+    }
+    
+    @objc func handleLongPressOnDeleteButton(_ gesture: UIGestureRecognizer) {
+        switch gesture.state {
+        case .began:
+            setUpTimer(with: #selector(handleSimpleDeleteEvent))
+        case .ended, .cancelled, .failed:
+            invalidateTimer()
+        default:
+            break
+        }
     }
     
 }
